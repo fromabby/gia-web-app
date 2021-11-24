acf5e9c69cb7edcb95677ecd5df588eb4ba46c0c
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-11-24 15:47:10 +0800

    update sidebar

--- components/template/sidebar.php
+++ components/template/sidebar.php
@@ -7,58 +7,67 @@
     <link href="../../css/sidebar-1.css" rel="stylesheet">
 
     <style>
-        /* Shrinking the sidebar from 250px to 80px and center aligining its content*/
-        #sidebar.active {
-            min-width: 80px;
-            max-width: 80px;
-            text-align: center;
+        /* Fixed sidenav, full height */
+        .sidenav {
+            height: 100%;
+            width: 200px;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #343A40;
+            overflow-x: hidden;
+            padding-top: 50px;
         }
 
-        /* Toggling the sidebar header content, hide the big heading [h3] and showing the small heading [strong] and vice versa*/
-        #sidebar .sidebar-header strong {
-            display: none;
-        }
-
-        #sidebar.active .sidebar-header h3 {
-            display: none;
-        }
-
-        #sidebar.active .sidebar-header strong {
+        /* Style the sidenav links and the dropdown button */
+        .sidenav a,
+        .dropdown-btn {
+            padding: 6px 8px 6px 16px;
+            text-decoration: none;
+            font-size: 20px;
+            color: #fff;
             display: block;
-        }
-
-        #sidebar ul li a {
+            border: none;
+            background: none;
+            width: 100%;
             text-align: left;
+            cursor: pointer;
+            outline: none;
         }
 
-        #sidebar.active ul li a {
-            padding: 20px 10px;
-            text-align: center;
-            font-size: 0.85em;
+        /* On mouse-over */
+        .sidenav a:hover,
+        .dropdown-btn:hover {
+            color: #f1f1f1;
         }
 
-        #sidebar.active ul li a i {
-            margin-right: 0;
-            display: block;
-            font-size: 1.8em;
-            margin-bottom: 5px;
+        /* Main content */
+        .main {
+            margin-left: 200px;
+            /* Same as the width of the sidenav */
+            font-size: 20px;
+            /* Increased text to enable scrolling */
+            padding: 0px 10px;
         }
 
-        /* Same dropdown links padding*/
-        #sidebar.active ul ul a {
-            padding: 10px !important;
+        /* Add an active class to the active dropdown button */
+        .active {
+            background-color: #785382;
+            color: black;
         }
 
-        /* Changing the arrow position to bottom center position, 
-   translateX(50%) works with right: 50% 
-   to accurately  center the arrow */
-        #sidebar.active .dropdown-toggle::after {
-            top: auto;
-            bottom: 10px;
-            right: 50%;
-            -webkit-transform: translateX(50%);
-            -ms-transform: translateX(50%);
-            transform: translateX(50%);
+        /* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+        .dropdown-container {
+            display: none;
+            background-color: #262626;
+            padding-left: 8px;
+        }
+
+        /* Optional: Style the caret down icon */
+        .fa-caret-down {
+            float: right;
+            padding-right: 8px;
         }
     </style>
 
@@ -73,51 +82,38 @@
 </head>
 
 <body>
-    <div class="wrapper">
-        <!-- Sidebar  -->
-        <nav id="sidebar" style="background-color:#343A40">
-            <div class="sidebar-header">
-            </div>
-            <ul class="list-unstyled components">
-                <li>
-                    <a href="dashboard.php">
-                        <i class="fas fa-columns"></i>
-                        Dashboard
-                    </a>
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" style="background-color:#343A40">
-                        <i class="fas fa-home"></i>
-                        Properties
-                    </a>
-                    <ul class="collapse list-unstyled" id="homeSubmenu" >
-                        <li>
-                            <a href="new_property.php"><i class="fas fa-plus mr-2"></i>Create Property</a>
-                        </li>
-                        <li>
-                            <a href="properties_table.php"><i class="far fa-trash-alt mr-2"></i>List Property</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="inquiries_table.php">
-                        <i class="far fa-question-circle"></i> Inquiries
-                    </a>
-                    <a href="admin_logout.php">
-                        <i class="fas fa-sign-out-alt"></i>
-                        Sign out
-                    </a>
-                    <a href="home.php">
-                        <i class="fas fa-briefcase"></i>
-                        Back to Home
-                    </a>
-                </li>
-            </ul>
-        </nav>
+    <div class="sidenav">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+        <button class="dropdown-btn">Dropdown
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+        <a href="#contact">Search</a>
     </div>
-    </nav>
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <!-- Popper.JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script>
+        //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
+    </script>
+
 </body>
