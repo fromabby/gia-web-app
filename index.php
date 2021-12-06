f62db2d23a4d14ac2a34aedb34622ac8f82e5150
fromabby <60252891+fromabby@users.noreply.github.com>
2021-12-06 20:12:51 +0800

    update

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-    $title = "Home";
-    require_once "./includes/header.php";
+$title = "Home";
+require_once "./includes/header.php";
 
-    include_once "./db/property.php";
-    //store list of properties in variable
-    $listProperty = $obj->getProperties();
+include_once "./db/property.php";
+//store list of properties in variable
+$listProperty = $obj->getProperties();
 ?>
+
 <head>
     <link href="./assets/css/home.css" rel="stylesheet">
 </head>
@@ -21,95 +22,96 @@
     </div>
 </div>
 <div class="pad">
-<div class="rectangle"></div>
-<div class="container-fluid secondSection">
-    <div class="row align-items-start">
-        <div class="col">
-            <h6>WHAT WE DO</h6>
-            <h3>GIA Homes is the Philippines’ leading innovative real estate developer.</h3>
-            <h5>GIA Homes offers a vibrant portfolio of groundbreaking 
-            real estate developments that provides upscale living and working spaces within 
-            various thriving and emerging growth centers around the country.</h5>
+    <div class="rectangle"></div>
+    <div class="container-fluid secondSection">
+        <div class="row align-items-start">
+            <div class="col">
+                <h6>WHAT WE DO</h6>
+                <h3>GIA Homes is the Philippines’ leading innovative real estate developer.</h3>
+                <h5>GIA Homes offers a vibrant portfolio of groundbreaking
+                    real estate developments that provides upscale living and working spaces within
+                    various thriving and emerging growth centers around the country.</h5>
+            </div>
+            <div class="col"><img class="img-fluid" src="db/images/company.jpg"></div>
         </div>
-        <div class="col"><img class="img-fluid" src="db/images/company.jpg"></div>
-    </div>   
-</div>
-<div class="rectangle"></div>
-<div class="firstSection">
-<h2 class="title text-center">See GIA Homes properties</h2>
-    <div class="row align-items-center card-contain center">
-        
-        <?php 
-        $x = 3;
-        while($x != 0 && $row = $listProperty->fetch_assoc()) {
-                echo "<div class='col col-12 col-sm-12 col-md-4'>" .
-                "<div class='text-center' style='margin: 20px auto; height: 40vh; width: 90%'>" .
-                    "<div class='card shadow text-center'>" .
-                        "<img class='service-icons align-items-center' src=./db/images/" . $row['image'] .  " height=200 >" .
-                        "<div class='card-body'>" .
-                            "<h4 class='card-title'>" . $row['name'] . "</h4>" .
-                            "<a href='property.php?id=" . $row['id'] . "' class='btn home-btns'>View</a>" .
-                        "</div>" .
-                    "</div>" . 
-                "</div>" .
-            "</div>"; 
-            $x=$x-1;
-        } 
-        ?>
     </div>
+    <div class="rectangle"></div>
+    <div class="firstSection">
+        <h2 class="title text-center">See GIA Homes properties</h2>
+        <div class="row align-items-center card-contain center">
 
-    </div>
-<div class="container-fluid fourthSection">
-    <div class="row align-items-start">
-        <div class="col">
-        <h3>Our Mission</h3>
-        </br></br></br></br>
-        <h3>Our Vision</h3>
+            <?php
+            $x = 3;
+            while ($x != 0 && $row = $listProperty->fetch_assoc()) {
+                echo "<div class='col col-12 col-sm-12 col-md-4'>" .
+                    "<div class='text-center' style='margin: 20px auto; height: 40vh; width: 90%'>" .
+                    "<div class='card shadow text-center'>" .
+                    "<img class='service-icons align-items-center' src=./db/images/" . $row['image'] .  " height=200 >" .
+                    "<div class='card-body'>" .
+                    "<h4 class='card-title'>" . $row['name'] . "</h4>" .
+                    "<a href='property.php?id=" . $row['id'] . "' class='btn home-btns'>View</a>" .
+                    "</div>" .
+                    "</div>" .
+                    "</div>" .
+                    "</div>";
+                $x = $x - 1;
+            }
+            ?>
         </div>
-        <div class="col-10">
-            
-        <h5>We strive to elevate our customers’ quality of life through innovative real estate solutions 
-                in vibrant growth centers all over the country. We act responsibly with integrity, accountability 
-                and total commitment. We achieve excellence through passion, focus and foresight.</h5>
+    </div>
+    <div class="container-fluid fourthSection">
+        <div class="row align-items-start">
+            <div class="col">
+                <h3>Our Mission</h3>
+                </br></br></br></br>
+                <h3>Our Vision</h3>
+            </div>
+            <div class="col-10">
+
+                <h5>We strive to elevate our customers’ quality of life through innovative real estate solutions
+                    in vibrant growth centers all over the country. We act responsibly with integrity, accountability
+                    and total commitment. We achieve excellence through passion, focus and foresight.</h5>
                 </br></br>
-           
-            <h5>We are the Philippines' leading innovative real estate developer serving the upscale market.</h5>
-   
-        </div>
-            
-             </div>
-</div>
-<div class="container-fluid thirdSection">
-    <h3>Our Workspace Portfolio</h3>
-    <div class="row cent align-items-start">
-        <div class="col">
-            <h1>16,643</h1>
-            <h5>Workstations</h5>
-        </div>
-        <div class="col">
-            <h1>30+</h1>
-            <h5>Office Locations</h5>
-        </div>
-        <div class="col">
-            
-            <h1>51</h1>
-            <h5>Managed Floors</h5>
-        </div>
-        <div class="col">
-        <h1>88,742 SQM</h1>
-            <h5>Total Workspace</h5p>
+
+                <h5>We are the Philippines' leading innovative real estate developer serving the upscale market.</h5>
+
+            </div>
+
         </div>
     </div>
-</div>
+    <div class="container-fluid thirdSection">
+        <h3>Our Workspace Portfolio</h3>
+        <div class="row cent align-items-start">
+            <div class="col">
+                <h1>16,643</h1>
+                <h5>Workstations</h5>
+            </div>
+            <div class="col">
+                <h1>30+</h1>
+                <h5>Office Locations</h5>
+            </div>
+            <div class="col">
+
+                <h1>51</h1>
+                <h5>Managed Floors</h5>
+            </div>
+            <div class="col">
+                <h1>88,742 SQM</h1>
+                <h5>Total Workspace</h5p>
+            </div>
+        </div>
+    </div>
 
 
     <div class="last-text">
-        
-    <center><p> GIA Homes is committed to giving our customers a secure and lasting agreements regarding houses they are interested in buying, selling, or renting.digital accessibility for individuals with disabilities.  
-        We are continuously working to improve the accessibility of our services for everyone. Feedback or reports of an issue will be highly appreciated and treated with utmost importance. </p></center>
+
+        <center>
+            <p> GIA Homes is committed to giving our customers a secure and lasting agreements regarding houses they are interested in buying, selling, or renting.digital accessibility for individuals with disabilities.
+                We are continuously working to improve the accessibility of our services for everyone. Feedback or reports of an issue will be highly appreciated and treated with utmost importance. </p>
+        </center>
     </div>
 </div>
 <div class="rectangle"></div>
 <?php
-	require_once "./includes/footer.php";
-?> 
+require_once "./includes/footer.php";
+?>
--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
-    $title = "Home";
-    require_once "../includes/header.php";
+$title = "Home";
+require_once "../includes/header.php";
 ?>
+
 <head>
     <link href="../assets/css/home.css" rel="stylesheet">
 </head>
@@ -17,55 +18,38 @@
     </div>
 </div>
 <div class="pad">
-<div class="rectangle"></div>
-<h2 class="title text-center">See the services of GIA Homes:</h2>
-    <div class="row align-items-center card-contain center">
-        <div class="col col-12 col-sm-12 col-md-4">
-            <div class="text-center" style="margin: 20px auto; height: 40vh; width: 90%">
-                <div class="card shadow text-center">
-                    <img class="service-icons align-items-center" src="../db/images/real-estate.png" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Buy a home</h4>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn home-btns">Search Homes</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="rectangle2"></div>
-        <div class="col col-12 col-sm-12 col-md-4">
-            <div class="text-center" style="margin: 20px auto; height: 40vh; width: 90%">
-                <div class="card shadow text-center">
-                <img class="service-icons align-items-center" src="../db/images/sell.png" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Sell a home</h4>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn home-btns">Give a Home</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col col-12 col-sm-12 col-md-4">
-            <div class="text-center" style="margin: 20px auto; height: 40vh; width: 90%">
-                <div class="card shadow text-center">
-                <img class="service-icons align-items-center" src="../db/images/search1.png" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Rent a home</h4>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn home-btns">Find Rentals</a>
-                    </div>
-                    
-                </div>
-            </div>
+    <div class="rectangle"></div>
+    <h2 class="title text-center">See the services of GIA Homes:</h2>
+    <div class="firstSection">
+        <h2 class="title text-center">See GIA Homes properties</h2>
+        <div class="row align-items-center card-contain center">
+
+            <?php
+            $x = 3;
+            while ($x != 0 && $row = $listProperty->fetch_assoc()) {
+                echo "<div class='col col-12 col-sm-12 col-md-4'>" .
+                    "<div class='text-center' style='margin: 20px auto; height: 40vh; width: 90%'>" .
+                    "<div class='card shadow text-center'>" .
+                    "<img class='service-icons align-items-center' src=./db/images/" . $row['image'] .  " height=200 >" .
+                    "<div class='card-body'>" .
+                    "<h4 class='card-title'>" . $row['name'] . "</h4>" .
+                    "<a href='property.php?id=" . $row['id'] . "' class='btn home-btns'>View</a>" .
+                    "</div>" .
+                    "</div>" .
+                    "</div>" .
+                    "</div>";
+                $x = $x - 1;
+            }
+            ?>
         </div>
     </div>
     <div class="last-text">
-        
-    <p> GIA Homes is committed to giving our customers a secure and lasting agreements regarding houses they are interested in buying, selling, or renting.digital accessibility for individuals with disabilities.  
-        We are continuously working to improve the accessibility of our services for everyone. Feedback or reports of an issue will be highly appreciated and treated with utmost importance. </p>
+
+        <p> GIA Homes is committed to giving our customers a secure and lasting agreements regarding houses they are interested in buying, selling, or renting.digital accessibility for individuals with disabilities.
+            We are continuously working to improve the accessibility of our services for everyone. Feedback or reports of an issue will be highly appreciated and treated with utmost importance. </p>
     </div>
 </div>
 <div class="rectangle"></div>
 <?php
-	require_once "../includes/footer.php";
+require_once "../includes/footer.php";
 ?>
